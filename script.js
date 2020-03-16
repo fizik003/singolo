8cbb133e3489584989bc39d5ee3263d6fae156fa
fizik003 <kassik0boks@gmail.com>
2020-03-16 15:35:23 +0300

    feat: add slider and of/on phone

--- script.js
+++ script.js
@@ -7,3 +7,42 @@ MENU.addEventListener('click', (event) => {
     event.target.classList.add('active');
     
 })
+
+
+
+// slider
+
+const SLIDES = document.querySelectorAll('#slides .slide');
+const ARROWS = document.querySelectorAll('.arrow');
+const SLIDER_SECTION = document.querySelector('#slider-section')
+let currentSlide = 0;
+
+function slider(){
+    SLIDES[currentSlide].classList.remove('showing');
+    currentSlide = (currentSlide + 1) % SLIDES.length;
+    SLIDES[currentSlide].classList.add('showing');
+    if(SLIDES[currentSlide].classList.contains('slide-blue')){
+        SLIDER_SECTION.style.borderBottomColor = '#5173cb';
+    }
+    else{
+        SLIDER_SECTION.style.borderBottomColor = '#ea676b';
+    }
+}
+
+// of/on screen
+
+const PHONE_BTNS = document.querySelectorAll('.phone-btn');
+const BLACK_SCREENS = document.querySelectorAll('.screen-black');
+
+function offScreen1(){
+    if(BLACK_SCREENS[0].classList.contains('show-black-screen'))
+    BLACK_SCREENS[0].classList.remove('show-black-screen');
+    else
+    BLACK_SCREENS[0].classList.add('show-black-screen');
+}
+function offScreen2(){
+    if(BLACK_SCREENS[1].classList.contains('show-black-screen'))
+    BLACK_SCREENS[1].classList.remove('show-black-screen');
+    else
+    BLACK_SCREENS[1].classList.add('show-black-screen');
+}
